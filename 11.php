ea50d177732bfa547f2ec0c962a16ae6a97c9615
redskycoder <mowcse@yahoo.com>
2020-02-28 21:02:44 +0600

    mou

---
+++ 11.php
@@ -0,0 +1,17 @@
+
+
+ <body>
+<?php
+if(isset($_POST['submit'])){
+	$tmp=$_FILES['image']['tmp_name'];
+	$img_name=uniqid().".jpg";
+	move_uploaded_file($tmp,"photos/".$img_name);
+
+}
+?>
+<form method="POST" action="" enctype="multipart/form-data">
+   <input type="file" name ="image">
+   <input type="submit"name="submit" value="upload">
+   </form>
+   </body>
+   </html>
